d7fc4c0e768a58fd29a83d560a998013a739cebf
Zach Smith <zsmith126028@gmail.com>
2019-02-14 16:04:16 -0600

    require batterProj, add batting projections route

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+// require models
+let BatterProjections = require('./models/batterProj');
+
 const port = 3000;
 
 mongoose.connect("mongodb://localhost:27017/baseballstats", {useNewUrlParser: true});
@@ -14,5 +17,16 @@ app.get('/', (req, res) => {
   res.render("landing");
 });
 
+// batting projections route
+app.get('/battingprojections', (req, res) => {
+  // get all batter projections from the database
+  BatterProjections.find({}, (err, allBatters) => {
+    if(err) {
+      console.log(err);
+    } else {
+      res.render("batterProjections", {batters: allBatters});
+    }
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
