c20e72ffa1318f40f0db3bae52af412b9b92c191
Zach Smith <zsmith126028@gmail.com>
2019-02-14 14:28:24 -0600

    add mongoose connection to mongodb

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const port = 3000;
 
+mongoose.connect("mongodb://localhost:27017/baseballstats", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
