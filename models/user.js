48e93d0d0bae7b1271d35d6ad52af3e5305eff8e
Zach Smith <zsmith126028@gmail.com>
2019-02-22 17:05:22 -0600

    add model for users

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+}, {collection: 'users'});
+
+module.exports = mongoose.model("User", UserSchema);
