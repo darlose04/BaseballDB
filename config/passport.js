b9b39e59c4ab5bec2a03c04773852a97ec3dfc69
Zach Smith <zsmith126028@gmail.com>
2019-02-22 20:05:20 -0600

    export passport in function that checks user auth

--- config/passport.js
+++ config/passport.js
@@ -5,3 +5,28 @@ const bcrypt = require('bcryptjs');
 // load user model
 const User = require('../models/user');
 
+module.exports = function(passport) {
+  passport.use(
+    new LocalStrategy({ usernameField: 'username' }, (username, password, done) => {
+      // match user
+      User.findOne({ username: username })
+        .then(user => {
+          if (!user) {
+            return done(null, false, { message: 'That username is not registered'});
+          }
+
+          // match password
+          bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) throw err;
+
+            if (isMatch) {
+              return done(null, user);
+            } else {
+              return done(null, false, {message: 'Incorrect Password'});
+            }
+          });
+        })
+        .catch(err => console.log(err));
+    });
+  );
+}
