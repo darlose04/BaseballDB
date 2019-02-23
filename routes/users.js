7fb683a0a2e660a95bb2b083000239a1b922b45c
Zach Smith <zsmith126028@gmail.com>
2019-02-22 19:18:45 -0600

    require bcryptjs

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 // User model required here when necessary
 const User = require('../models/user');
