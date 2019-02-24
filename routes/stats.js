4babe20d9034bccd5d5a1be908adbdd50a87b33e
Zach Smith <zsmith126028@gmail.com>
2019-02-24 13:03:44 -0600

    remove middleware for dev convenience

--- routes/stats.js
+++ routes/stats.js
@@ -10,8 +10,12 @@ let PitcherProjections = require('../models/pitcherProj');
 let Pitchers2018 = require('../models/pitchers2018');
 let Pitchers2017 = require('../models/pitchers2017');
 
+// ==============================
+// REMEMBER TO ADD ENSUREAUTHENTICATED MIDDLEWARE TO ROUTES BEFORE PUSHING TO HEROKU
+//===============================
+
 // batting projections route
-router.get('/battingprojections', ensureAuthenticated, (req, res) => {
+router.get('/battingprojections', (req, res) => {
   // get all batter projections from the database
   BatterProjections.find({}, (err, allBatters) => {
     if(err) {
@@ -23,7 +27,7 @@ router.get('/battingprojections', ensureAuthenticated, (req, res) => {
 });
 
 // batting 2018 stats route
-router.get('/batting2018', ensureAuthenticated, (req, res) => {
+router.get('/batting2018', (req, res) => {
   Batters2018.find({}, (err, all2018Batters) => {
     if(err) {
       console.log(err);
@@ -34,7 +38,7 @@ router.get('/batting2018', ensureAuthenticated, (req, res) => {
 });
 
 // batting 2017 stats route
-router.get('/batting2017', ensureAuthenticated, (req, res) => {
+router.get('/batting2017', (req, res) => {
   Batters2017.find({}, (err, all2017Batters) => {
     if(err) {
       console.log(err);
@@ -45,7 +49,7 @@ router.get('/batting2017', ensureAuthenticated, (req, res) => {
 });
 
 // pitching projections route
-router.get('/pitchingprojections', ensureAuthenticated, (req, res) => {
+router.get('/pitchingprojections', (req, res) => {
   // get all pitcher projections from the database
   PitcherProjections.find({}, (err, allPitchers) => {
     if(err) {
@@ -57,13 +61,19 @@ router.get('/pitchingprojections', ensureAuthenticated, (req, res) => {
 });
 
 // pitching 2018 stats route
-router.get('/pitching2018', ensureAuthenticated, (req, res) => {
+router.get('/pitching2018', (req, res) => {
   // get all 2018 pitching from the database (not in db yet though)
-  res.render("pitchers2018");
+  Pitchers2018.find({}, (err, all2018Pitchers) => {
+    if(err) {
+      console.log(err);
+    } else {
+      res.render("pitchers2018", {pitchers2018: all2018Pitchers});
+    }
+  });
 });
 
 // pitching 2017 stats route
-router.get('/pitching2017', ensureAuthenticated, (req, res) => {
+router.get('/pitching2017', (req, res) => {
   // get all 2017 pitching from the database (not in db yet though)
   res.render("pitchers2017");
 });
