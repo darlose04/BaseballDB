8dec4243f4484a915302c1f6149440752a92e875
Zach Smith <zsmith126028@gmail.com>
2019-03-05 14:49:25 -0600

    add dashboard route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
 // root route
 router.get('/', (req, res) => {
   res.render('landing', {
     // username: req.user.username
   })
+});
+
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+  res.render('dashboard', {
+    username: req.user.username
+  });
 });
 
 module.exports = router;
