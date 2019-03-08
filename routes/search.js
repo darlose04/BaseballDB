cd1d0b25c59fb6fcff4d1fcd3efc94a873781532
Zach Smith <zsmith126028@gmail.com>
2019-03-07 22:50:55 -0600

    trying to create a route that allows a user to search stats and return all players that qualify. It is not working

--- routes/search.js
+++ routes/search.js
@@ -10,3 +10,40 @@ let PitcherProjections = require('../models/pitcherProj');
 // REMEMBER TO ADD ENSUREAUTHENTICATED MIDDLEWARE TO ROUTES BEFORE PUSHING TO HEROKU
 //===============================
 
+// batting search route
+router.get('/batting', (req, res) => {
+  // get data from form
+  let runs = req.body.runs;
+  let homeruns = req.body.homeruns;
+  let rbi = req.body.rbi;
+  let sb = req.body.sb;
+  let ops = req.body.ops;
+
+
+  BatterProjections.find({
+    R: {$gte: runs}, 
+    HR: {$gte: homeruns},
+    RBI: {$gte: rbi},
+    SB: {$gte: sb},
+    OPS: {$gte: ops}
+  }, (err, allQualifiedBatters) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("batterSearch", {batters: allQualifiedBatters});
+    }
+  });
+  
+
+  
+  
+});
+
+
+
+// pitching search route
+router.get('/pitching', (req, res) => {
+  res.render("pitcherSearch");
+});
+
+module.exports = router;
